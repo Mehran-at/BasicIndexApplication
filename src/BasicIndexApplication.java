130d77842c6fa0bc99e09add7d2a1fa664ca6874
Mehran <36104315+Mehran-at@users.noreply.github.com>
2018-11-26 17:03:27 +0100

    Update BasicIndexApplication.java

--- src/BasicIndexApplication.java
+++ src/BasicIndexApplication.java
@@ -1,3 +1,16 @@
+//Matilda just bought a new book about social impact and as she opens it, she sees the index
+//        . Create the BasicIndexApplication class and write the following instructions in its main method:
+//        . Create an empty index map connecting Integers for the page numbers and Strings for the book topics and display it.
+//        . Create three Integers that represent three different page numbers.
+//        . Create three Strings that represent three different book topics.
+//        . Put them into the index map from biggest to smallest page number and display it to notice that the entries are automatically arranged.
+//        . Display the map keys.
+//        . Display the map values.
+//        . Create one String representing another book topic.
+//        . Put this new topic into the same page number than an existing one and display the map to notice that the previous one is replaced.
+//        . Ask if one of the keys is contained in the map and display the answer.
+//        . Get one of them by providing its name as an argument and display it.
+//        . Display its size.
 import java.util.*;
 public class BasicIndexApplication {
     public static void main(String[] args) {
